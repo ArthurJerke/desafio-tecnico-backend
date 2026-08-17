978a6689a8aa9b26a18443f4969919ca7e6b4e19
ArthurJerke <a.valejerke@gmail.com>
2026-08-17 16:55:26 -0300

    Adicionando modulo de notícias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NoticiasModule } from './noticias/noticias.module';
 
 @Module({
-  imports: [],
+  imports: [NoticiasModule],
   controllers: [AppController],
   providers: [AppService],
 })
